b911d83ea925e93b69cebb763a530caeb4f7f6e4
Kevin Kwan <KevinKwan95@googlemail.com>
2016-10-24 23:49:46 +0100

    Add initial attempt at parsing smt value data

---
+++ util/smt-parser.js
@@ -0,0 +1,49 @@
+function findClosingParen(text, start) {
+	let end = start;
+	let count = 1;
+
+	while (count > 0) {
+		const c = text[++end];
+
+		if (c === '(') {
+			count++;
+		}
+		else if (c === ')') {
+			count--;
+		}
+	}
+
+	return end;
+}
+
+function parseValues(text) {
+	// Strip outer parentheses.
+	text = text.slice(1, text.length - 1);
+
+	const values = [];
+	// Find the first occurance of a space and split on that to determine name and value.
+	const objectify = (s) => {
+		const index = s.indexOf(' ');
+		const name = s.substring(0, index);
+		const value = s.substring(index + 1);
+
+		return { [name]: value };
+	};
+
+	for (var i = 0; i < text.length; i++) {
+		if (text[i] === '(') {
+			i++;
+			const oppositeParen = findClosingParen(text, i);
+			const valueString = text.slice(i, oppositeParen);
+
+			values.push(objectify(valueString));
+			i = oppositeParen;
+		}
+	}
+
+	return Object.assign.apply(null, values);
+}
+
+module.exports = {
+	parseValues
+}
