b0eea0b65c959fc819a4ef6b49ac46f6138fbf45
Kevin Kwan <KevinKwan95@googlemail.com>
2016-10-30 23:43:11 +0000

    Useful comments in install.sh

--- scripts/install.js
+++ scripts/install.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const spawn = require('child_process').spawnSync;
 
-// get library path
+// TODO: Keep updated with projects list.
 const projects = [
 	'smt-generator'
 ];
@@ -12,11 +12,13 @@ projects.map((project) => {
 	const projectPath = path.join(root, project);
 	const packageJson = path.join(projectPath, 'package.json');
 
+	// Confirm the directory has a package.json
 	fs.stat(packageJson, (err, stats) => {
 		if (err || stats.isDirectory()) {
 			return;
 		}
 
+		// Run `npm install` in project directory.
 		console.log(`[npm Install] ${project}`);
 		spawn('npm', ['install'], {
 			env: process.env,
