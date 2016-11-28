4d27b5c24199483d7af1b86bc4e8d91406c478dc
Kevin Kwan <KevinKwan95@googlemail.com>
2016-11-28 20:30:08 +0000

    Generate test suite in test-script

--- test/test-script.js
+++ test/test-script.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const spawn = require('child_process').spawn;
 
+console.log("Parse UML");
 const umlParser = require('../uml-parser/index.js');
 umlParser(path.join(__dirname, '../build/AnnotatedSimpleMath.uml'));
 
 setTimeout(() => {
+	console.log("Create SMT");
 	// Take the annotated UML and create SMT files under 'build'.
 	const SmtClass = require('../smt-generator/util/class.js');
 	const data = require('../build/SimpleMath.json');
@@ -11,8 +14,23 @@ setTimeout(() => {
 	umlToSmt.writeSMT(path.join(__dirname, '../build/'));
 
 	setTimeout(() => {
+		console.log("Solve SMT");
 		// Solves all SMT files to 'build/solved.json'.
 		const z3Solver = require('../smt-generator/util/smt-solve.js');
 		z3Solver.solve();
+
+		setTimeout(() => {
+			console.log("Generate Test Suite");
+			// Invoke generator.
+			const yo = spawn('yo', ['umltotest:nunit', 'build/SimpleMath.json', 'build/solved.json']);
+
+			yo.stdout.on('data', (data) => {
+				console.log(data.toString('utf8'));
+			});
+
+			yo.stderr.on('data', (data) => {
+				console.log(data.toString('utf8'));
+			});
+		}, 500);
 	}, 500);
 }, 500);
