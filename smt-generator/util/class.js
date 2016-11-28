5191d5f50762510370a0e8ec36c3041c12070545
Kevin Kwan <KevinKwan95@googlemail.com>
2016-11-28 19:06:25 +0000

    Update smt-generator to work with new data schema

--- smt-generator/util/class.js
+++ smt-generator/util/class.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 
 class SmtClass {
 	constructor(classObject) {
-		this.smtCommands = classObject.methods.map((m) => {
-			const method = new SmtMethod(m);
-			const commands = method.getCommands().join('\n');
+		this.smtCommands = Object.keys(classObject.methods).map((m) => {
+			const method = classObject.methods[m];
+			const smtMethod = new SmtMethod(method);
+			const commands = smtMethod.getCommands().join('\n');
 
 			return {
-				name: m.name,
+				name: method.name,
 				commands: commands
 			};
 		});
