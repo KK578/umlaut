07b875e1e2c1b7abf81b9da3ffb334016fa3cd60
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-15 15:59:46 +0000

    Test stubs for Boolean Expression

--- test/smt-generator/classes-test.js
+++ test/smt-generator/classes-test.js
@@ -5,6 +5,24 @@ const testee = require('../../src/smt-generator/util/smt-classes.js');
 let TestClass;
 
 describe('SMT Classes', () => {
+	describe('Boolean Expression', () => {
+		before(() => {
+			TestClass = testee.BooleanExpression;
+		});
+
+		it('should error on empty input', () => {
+			expect(() => {
+				new TestClass();
+			}).to.throw(Error);
+		});
+
+		it('should take a comparison and argument list');
+		it('should take a comparison and argument list containing BooleanExpressions');
+		it('should do a lookup to convert a comparison to SMT-LIB2 version');
+		it('should change isInverted with setInverted');
+		it('should represent SMT-LIB2 string differently if inverted');
+	});
+
 	describe('Assertion', () => {
 		before(() => {
 			TestClass = testee.Assertion;
