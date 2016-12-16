a1a1a1f61026aadbf206fd2db1689ec7da85bd22
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-16 16:34:00 +0000

    Setup coverage reports correctly for CI

--- grunt/mocha_istanbul.js
+++ grunt/mocha_istanbul.js
@@ -1,6 +1,15 @@
 module.exports = {
-	options: { reporter: 'spec' },
+	options: {
+		reporter: 'spec',
+		reportFormats: ['lcovonly']
+	},
 	'all': { src: 'test/*/*-test.js' },
+	'html': {
+		options: {
+			reportFormats: ['lcov']
+		},
+		src: 'test/*/*-test.js'
+	},
 	'uml-parser': { src: 'test/uml-parser/*-test.js' },
 	'smt-generator': { src: 'test/smt-generator/*-test.js' },
 	'test-generator': { src: 'test/test-generator/*-test.js' }
