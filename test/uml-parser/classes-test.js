96e0bfca96fe2c5324d78ec97d6a1062b1aa234b
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-17 00:33:14 +0000

    Add test code stubs for future UML-Parser classes

---
+++ test/uml-parser/classes-test.js
@@ -0,0 +1,101 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const testee = require('../../src/uml-parser/util/classes.js');
+let TestClass;
+
+describe('UML Parser Classes', () => {
+	describe('AnnotatedUmlClass', () => {
+		it('should error on empty input', () => {
+			expect(() => {
+				new TestClass();
+			}).to.throw(Error);
+		});
+
+		it('should take a name', () => {
+			const obj = new TestClass('foo');
+
+			expect(obj.id).to.be.a('string').and.not.equal('');
+			expect(obj.name).to.equal('foo');
+		});
+
+		describe('Class Variable Modifiers', () => {
+			describe('#addVariable', () => {
+				it('should error on empty input');
+				it('should take a name');
+				it('should take a name and type');
+				it('should error if variable name already exists');
+			});
+
+			describe('#addInvariant', () => {
+				it('should error on empty');
+				it('should add a new invariant, as a string, to the class');
+				it('should error if invariant string is not bracketed');
+				it('should error if invariant string does not specify the comparator');
+				it('should add a new invariant, as an object, to the class');
+				it('should error if invariant object does not specify the comparator');
+				it('should error if invariant object does not specify at least 1 item in arguments');
+			});
+		});
+
+		describe('Class Method Modifiers', () => {
+			let obj;
+
+			beforeEach(() => {
+				obj = new TestClass();
+			});
+
+			describe('#addMethod', () => {
+				it('should error on empty input');
+				it('should error if list of arguments is not an array');
+				it('should take a name');
+				it('should take a name and type');
+				it('should take a name and type and list of arguments');
+				it('should error if method name already exists');
+			});
+
+			describe('#setMethodType', () => {
+				it('should error on empty');
+				it('should error with just a name');
+				it('should change the named method\'s return type');
+				it('should error if method does not exist');
+			});
+
+			describe('#addMethodArgument', () => {
+				it('should error on empty');
+				it('should error with just a name');
+				it('should add a new argument, as a string, to the named method');
+				it('should error if argument string does not specify a type');
+				it('should add a new argument, as an object, to the named method');
+				it('should error if added argument does not have a name');
+				it('should error if added argument does not have a type');
+				it('should error if method does not exist');
+				it('should error if argument name already exists in that method');
+			});
+
+			describe('#addMethodPrecondition', () => {
+				it('should error on empty');
+				it('should error with just a name');
+				it('should add a new precondition, as a string, to the named method');
+				it('should error if precondition string is not bracketed');
+				it('should error if precondition string does not specify the comparator');
+				it('should add a new precondition, as an object, to the named method');
+				it('should error if precondition object does not specify the comparator');
+				it('should error if precondition object does not specify at least 1 item in arguments');
+				it('should error if method does not exist');
+			});
+
+			describe('#addMethodPostcondition', () => {
+				it('should error on empty');
+				it('should error with just a name');
+				it('should add a new precondition, as a string, to the named method');
+				it('should error if precondition string is not bracketed');
+				it('should error if precondition string does not specify the comparator');
+				it('should add a new precondition, as an object, to the named method');
+				it('should error if precondition object does not specify the comparator');
+				it('should error if precondition object does not specify at least 1 item in arguments');
+				it('should error if method does not exist');
+			});
+		});
+	});
+});
