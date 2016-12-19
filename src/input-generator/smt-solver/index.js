f9fdd4b788d22d06794fdbd1ee596db4a27273ca
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-19 15:51:19 +0000

    Rework uml-to-smt to support new classes format

--- src/input-generator/smt-solver/index.js
+++ src/input-generator/smt-solver/index.js
@@ -5,29 +5,29 @@ const SmtClass = require('./uml-to-smt/class.js');
 
 function promiseFsWriteFile(filepath, data) {
 	return new Promise((resolve, reject) => {
-		fs.writeFile(filename, data, 'utf-8', (err) => {
+		fs.writeFile(filepath, data, 'utf-8', (err) => {
 			if (err) {
 				reject(err);
 			}
 			else {
 				resolve();
 			}
-		})
+		});
 	});
 }
 
 function promiseWriteAllFiles(dir, data, index) {
 	return new Promise((resolve, reject) => {
-		if (index >= data.length) {
+		if (index >= data.smtCommands.length) {
 			// All files are written.
 			resolve();
 		}
 		else {
-			const method = data[index];
-			const filePath = path.join(dir, `${method.name}.smt2`);
+			const method = data.smtCommands[index];
+			const filepath = path.join(dir, `${method.name}.smt2`);
 
 			return promiseFsWriteFile(filepath, method.commands).then(() => {
-				return promiseWriteAllFiles(dir, data, index + 1);
+				return promiseWriteAllFiles(dir, data, index + 1).then(resolve);
 			}).catch((err) => {
 				reject(err);
 			});
@@ -41,7 +41,7 @@ function parseUml(uml) {
 	return smtClass;
 }
 
-function writeSmt(smtClass) {
+function writeSmt(dir, smtClass) {
 	const classPath = path.join(dir, `smt/${smtClass.name}/`);
 
 	return promiseWriteAllFiles(classPath, smtClass, 0);
@@ -50,5 +50,5 @@ function writeSmt(smtClass) {
 module.exports = (uml) => {
 	const parsedUml = parseUml(uml);
 
-	return writeSmt(parsedUml);
+	return writeSmt('build/test/', parsedUml);
 };
