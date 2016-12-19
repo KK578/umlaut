074efbf501b141ff4f98089b5019425c5dd78985
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-19 02:22:43 +0000

    Run test-generator on SimpleMath fixture

--- test/test-generator/nunit-test.js
+++ test/test-generator/nunit-test.js
@@ -1,6 +1,19 @@
 const chai = require('chai');
+const helpers = require('yeoman-test');
+const path = require('path');
 const expect = chai.expect;
 
-describe('NUnit', () => {
-	it('should make a test');
+describe.only('model-driven-testing:nunit test/fixtures/test-generator/uml/ test/fixtures/test-generator/smt/', () => {
+	beforeEach(() => {
+		return helpers.run(path.join(__dirname, '../../generators/nunit'))
+			.inDir('test/tmp/')
+			.withArguments([
+				'../fixtures/test-generator/uml/',
+				'../fixtures/test-generator/smt/'
+			]);
+	});
+
+	it('should make a test', () => {
+		expect(1).to.equal(1);
+	});
 });
