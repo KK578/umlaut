2213fbe25be8a968ae85229c31583bebdb6ff5f2
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-19 21:39:55 +0000

    Disallow SmtAssertion to take a string input

--- input-generator/util/smt-solver/classes/Assertion.js
+++ input-generator/util/smt-solver/classes/Assertion.js
@@ -1,7 +1,13 @@
+const SmtBooleanExpression = require('./BooleanExpression');
+
 module.exports = class SmtAssertion {
 	constructor(expression) {
 		if (!expression) {
 			throw new Error('Argument "expression" is required.');
+		}
+
+		if (!(expression instanceof SmtBooleanExpression)) {
+			throw new Error('Expected "expression" to be a SmtBooleanExpression');
 		}
 
 		this.expression = expression;
