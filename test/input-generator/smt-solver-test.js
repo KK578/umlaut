a7f1105c7ba9aec45b21682a0c587b9b3d4cd297
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-19 14:54:14 +0000

    Add test runner for input-generator as is

---
+++ test/input-generator/smt-solver-test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const path = require('path');
+
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+const testee = require('../../src/input-generator/smt-solver/index.js');
+
+describe('Input Generator SMT Solver', () => {
+	it('should run against SimpleMath test fixture', () => {
+		const fixture = require(path.join(__dirname, '../../build/uml/SimpleMath.json'));
+		const promise = testee(fixture);
+
+		return expect(promise).to.be.fulfilled;
+	});
+});
