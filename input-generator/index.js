8b406514af4324c8b43c2b2bde4f2ecc8394d38c
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-19 23:05:53 +0000

    Remove old z3 code from top level input-generator index

--- input-generator/index.js
+++ input-generator/index.js
@@ -1,25 +1,3 @@
-const path = require('path');
-const fs = require('fs');
-
-const SmtClass = require('./smt-solver/uml-to-smt/class.js');
-
-function parseUml(uml) {
-	const smtClass = new SmtClass(uml);
-
-	return smtClass;
-}
-
-function write(smtClass, dir) {
-	const classPath = path.join(dir, `smt/${smtClass.name}/`);
-
-	smtClass.smtCommands.map((method) => {
-		const filePath = path.join(classPath, `${method.name}.smt2`);
-
-		fs.writeFile(filePath, method.commands);
-	});
-}
-
 module.exports = {
-	smtSolve: require('./smt-solver/index.js'),
-	write
+	smtSolve: require('./smt-solver/index.js')
 };
