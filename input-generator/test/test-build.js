ba25315c87fa34bc1bdccf843c2ba9edd74915d6
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-22 10:34:54 +0000

    Delete old test suite in input-generator

--- input-generator/test/test-build.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-const SmtClass = require('../util/class.js');
-const data = require('./test-data.json');
-
-const smt = new SmtClass(data);
-
-smt.writeSMT(path.join(__dirname, '../build/'));
