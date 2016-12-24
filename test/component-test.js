fd3eac052658f999ee9ec3b78284374e5a1a7fab
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-24 01:56:14 +0000

    Make visual studio uml-parser tests more specific

--- test/component-test.js
+++ test/component-test.js
@@ -6,20 +6,37 @@ chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
+const promises = require('../util/promises.js');
+
 describe('Component Tests', () => {
 	describe('uml-parser', () => {
 		describe('Visual Studio', () => {
 			let testee;
 
 			before(() => {
-				testee = require('../uml-parser/index.js');
+				testee = require('../uml-parser/parsers/visual-studio/index.js');
+			});
+
+			it('should state it can parse Visual Studio SimpleMath test fixture', () => {
+				const fixture = path.join(__dirname, './fixtures/SimpleMath/ModelDefinition/SimpleMath.uml');
+
+				return promises.fsReadFile(fixture).then((data) => {
+					const promise = testee.detect(data);
+
+					return expect(promise).to.be.fulfilled;
+				}).then((result) => {
+					expect(result).to.be.ok;
+				});
 			});
 
 			it('should parse Visual Studio SimpleMath test fixture', () => {
 				const fixture = path.join(__dirname, './fixtures/SimpleMath/ModelDefinition/SimpleMath.uml');
-				const promise = testee(fixture);
 
-				return expect(promise).to.be.fulfilled.then((result) => {
+				return promises.fsReadFile(fixture).then((data) => {
+					const promise = testee.parse(data);
+
+					return expect(promise).to.be.fulfilled;
+				}).then((result) => {
 					expect(result.SimpleMath).to.be.an('object');
 				});
 			});
@@ -28,6 +45,7 @@ describe('Component Tests', () => {
 
 	describe('input-generator', () => {
 		let testee;
+
 		describe('smt-solver', () => {
 			before(() => {
 				testee = require('../input-generator/smt-solver/index.js');
