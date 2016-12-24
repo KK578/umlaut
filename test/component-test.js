9fe232026c4f3899ac1be56f360afdfd8f1993d3
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-24 01:32:30 +0000

    Add top level components test

---
+++ test/component-test.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const path = require('path');
+
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+describe('Component Tests', () => {
+	describe('uml-parser', () => {
+		describe('Visual Studio', () => {
+			let testee;
+
+			before(() => {
+				testee = require('../uml-parser/index.js');
+			});
+
+			it('should parse Visual Studio SimpleMath test fixture', () => {
+				const fixture = path.join(__dirname, './fixtures/SimpleMath/ModelDefinition/SimpleMath.uml');
+				const promise = testee(fixture);
+
+				return expect(promise).to.be.fulfilled.then((result) => {
+					expect(result.SimpleMath).to.be.an('object');
+				});
+			});
+		});
+	});
+
+	describe('input-generator', () => {});
+	describe('test-generator', () => {})
+});
