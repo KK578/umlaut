06420391ea44d053f670ba9139d54f89a10d6b0e
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-24 01:45:19 +0000

    Move fsReadFile promise into util promises

--- uml-parser/index.js
+++ uml-parser/index.js
@@ -1,22 +1,9 @@
-const fs = require('fs');
+const promises = require('../util/promises.js');
 
 const parser = require('./parsers/parser.js');
 
-function promiseFsReadFile(filename) {
-	return new Promise((resolve, reject) => {
-		fs.readFile(filename, 'utf-8', (err, data) => {
-			if (err) {
-				reject(err);
-			}
-			else {
-				resolve(data);
-			}
-		});
-	});
-}
-
 module.exports = (filePath) => {
-	return promiseFsReadFile(filePath).then((data) => {
+	return promises.fsReadFile(filePath).then((data) => {
 		return parser.parse(data);
 	}).catch((error) => {
 		throw error;
