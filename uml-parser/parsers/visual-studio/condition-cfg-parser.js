9b78b983d257ceb80862c2827360be3fae204c26
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-25 02:20:05 +0000

    Copy value-cfg-parser for condition-cfg-parser in uml-parser

---
+++ uml-parser/parsers/visual-studio/condition-cfg-parser.js
@@ -0,0 +1,71 @@
+const parsey = require('parsey');
+
+const Sym = parsey.Sym;
+const Rule = parsey.Rule;
+
+/**
+ * CFG for conditions annotated on UML models.
+ *
+ * condition	::= '(' comparison argumentList ')'
+ *
+ * argumentList	::= argumentList argument
+ * 					| argument
+ *
+ * argument		::= [_a-zA-Z][-_a-zA-Z0-9]*
+ *
+ * exception	::= 'Exception' ':' argument
+ */
+const condition = new Sym('condition');
+const comparison = new Sym('comparison');
+const argumentList = new Sym('argumentList');
+const argument = new Sym('argument');
+const exception = new Sym('exception');
+
+const grammar = [
+	new Rule(condition, ['(', comparison, argumentList, ')'], (_, c, a) => {
+		return {
+			comparison: c,
+			arguments: a
+		};
+	}),
+
+	new Rule(comparison, ['LessThan'], (c) => {
+		return c;
+	}),
+
+	new Rule(argumentList, [argumentList, argument], (next, a) => {
+		return next.concat(a);
+	}),
+	new Rule(argumentList, [argument], (a) => {
+		return [a];
+	}),
+
+	new Rule(argument, [/[a-zA-Z_][-_a-zA-Z0-9]*/], (a) => {
+		return a;
+	}),
+];
+
+// Note: From parsey/examples/calc.
+function interpret(parseTree) {
+	if (typeof parseTree === 'string' || (parseTree === null || parseTree === undefined)) {
+		return parseTree;
+	}
+
+	const values = parseTree.children
+		.map(interpret)
+		.filter((value) => {
+			const result = !(value === null || value === undefined);
+
+			return result;
+		});
+
+	return parseTree.item.evaluate(values);
+}
+
+function parse(input) {
+	const parsed = parsey.parse(input, grammar);
+
+	return interpret(parsed);
+}
+
+module.exports = parse;
