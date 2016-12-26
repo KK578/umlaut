0bbdae997b7084fd24ef764ed8767f8d6ecf1224
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-26 20:37:49 +0000

    Add test case stubs

--- test/component-test.js
+++ test/component-test.js
@@ -15,34 +15,45 @@ const promises = require('../util/promises.js');
 
 describe('Component Tests', () => {
 	describe('Uml-Parser', () => {
-		describe('Visual Studio models', () => {
+		describe('Visual Studio Parser', () => {
 			let testee;
 
 			before(() => {
 				testee = require('../uml-parser/parsers/visual-studio/index.js');
 			});
 
-			it('should state it can parse Visual Studio SimpleMath test fixture', () => {
-				const fixture = path.join(__dirname, './fixtures/SimpleMath/ModelDefinition/SimpleMath.uml');
+			it('should fail to parse a non XML string');
+			it('should fail to parse an arbitrary XML string');
+			it('should parse if the XML root contains a "modelStoreModel" attribute');
+			it('should parse if the XML root contains a "logicalClassDesignerModel" attribute');
+			it('should parse Visual Studio model with class with no variables');
+			it('should parse Visual Studio model with class with no methods');
+			it('should parse Visual Studio model with no classes');
+			it('should parse Visual Studio model with multiple classes');
 
-				return promises.fsReadFile(fixture).then((data) => {
-					const promise = testee.detect(data);
+			describe('SimpleMath Test Fixture', () => {
+				it('should successfully be detected as parsable', () => {
+					const fixture = path.join(__dirname, './fixtures/SimpleMath/ModelDefinition/SimpleMath.uml');
 
-					return expect(promise).to.be.fulfilled;
-				}).then((result) => {
-					expect(result).to.be.ok;
+					return promises.fsReadFile(fixture).then((data) => {
+						const promise = testee.detect(data);
+
+						return expect(promise).to.be.fulfilled;
+					}).then((result) => {
+						expect(result).to.be.ok;
+					});
 				});
-			});
 
-			it('should parse Visual Studio SimpleMath test fixture', () => {
-				const fixture = path.join(__dirname, './fixtures/SimpleMath/ModelDefinition/SimpleMath.uml');
+				it('should successfully be parsed', () => {
+					const fixture = path.join(__dirname, './fixtures/SimpleMath/ModelDefinition/SimpleMath.uml');
 
-				return promises.fsReadFile(fixture).then((data) => {
-					const promise = testee.parse(data);
+					return promises.fsReadFile(fixture).then((data) => {
+						const promise = testee.parse(data);
 
-					return expect(promise).to.be.fulfilled;
-				}).then((result) => {
-					expect(result.SimpleMath).to.be.an('object');
+						return expect(promise).to.be.fulfilled;
+					}).then((result) => {
+						expect(result.SimpleMath).to.be.an('object');
+					});
 				});
 			});
 		});
