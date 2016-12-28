9edb3ff62bf7cf72ffd81cafcd34a27b41b8be65
Kevin Kwan <KevinKwan95@googlemail.com>
2016-12-28 01:06:43 +0000

    Add util/comparisons.js

---
+++ util/comparisons.js
@@ -0,0 +1,47 @@
+const comparisons = require('./comparisons.json');
+
+function toSmtSymbol(comparison) {
+	for (let i = 0; i < comparisons.length; i++) {
+		const c = comparisons[i];
+
+		if (c.name === comparison) {
+			const symbol = c.smtSymbol ? c.smtSymbol : c.symbol;
+
+			return symbol;
+		}
+	}
+
+	throw new Error(`Comparison with name "${comparison}" does not exist.`);
+}
+
+function toSymbol(comparison) {
+	for (let i = 0; i < comparisons.length; i++) {
+		const c = comparisons[i];
+
+		if (c.name === comparison) {
+			const symbol = c.smtSymbol ? c.smtSymbol : c.symbol;
+
+			return symbol;
+		}
+	}
+
+	throw new Error(`Comparison with name "${comparison}" does not exist.`);
+}
+
+function toName(comparison) {
+	for (let i = 0; i < comparisons.length; i++) {
+		const c = comparisons[i];
+
+		if (c.symbol === comparison) {
+			return c.name;
+		}
+	}
+
+	throw new Error(`Comparison with symbol "${comparison}" does not exist.`);
+}
+
+module.exports = {
+	toName,
+	toSymbol,
+	toSmtSymbol
+};
