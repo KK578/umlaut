d6d5effcb68552ca7ac0c12f90eac51f52fc433d
Kevin Kwan <KevinKwan95@googlemail.com>
2017-01-01 09:39:59 +0000

    Split tests by submodule

--- grunt/mochaTest.js
+++ grunt/mochaTest.js
@@ -1,6 +1,5 @@
 module.exports = {
 	options: { reporter: 'spec' },
-	'all': { src: 'test/test-suite.js' },
 	'uml-parser': { src: 'test/uml-parser/test-suite.js' },
 	'input-generator': { src: 'test/input-generator/test-suite.js' },
 	'test-generator': { src: 'test/test-generator/test-suite.js' }
--- grunt/mocha_istanbul.js
+++ grunt/mocha_istanbul.js
@@ -3,14 +3,22 @@ module.exports = {
 		reporter: 'spec',
 		reportFormats: ['lcovonly']
 	},
-	'all': { src: 'test/test-suite.js' },
-	'html': {
+	html: {
 		options: {
 			reportFormats: ['lcov']
 		},
 		src: 'test/test-suite.js'
 	},
-	'uml-parser': { src: 'test/uml-parser/test-suite.js' },
-	'input-generator': { src: 'test/input-generator/test-suite.js' },
-	'test-generator': { src: 'test/test-generator/test-suite.js' }
+	'uml-parser': {
+		options: { coverageFolder: 'coverage/uml-parser/' },
+		src: 'test/uml-parser/test-suite.js'
+	},
+	'input-generator': {
+		options: { coverageFolder: 'coverage/input-generator/' },
+		src: 'test/input-generator/test-suite.js'
+	},
+	'test-generator': {
+		options: { coverageFolder: 'coverage/test-generator/' },
+		src: 'test/test-generator/test-suite.js'
+	}
 };
