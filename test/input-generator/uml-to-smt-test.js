e0a88a6ed7ed1e245e894af687f5447646c955bc
Kevin Kwan <KevinKwan95@googlemail.com>
2017-01-01 02:42:34 +0000

    Add test for inverted equality

--- test/input-generator/uml-to-smt-test.js
+++ test/input-generator/uml-to-smt-test.js
@@ -54,10 +54,54 @@ describe('UML-To-SMT', () => {
 			expect(result.Test.smtCommands).to.be.instanceOf(Array).and.have.length(1);
 			expect(result.Test.smtCommands[0]).to.have.keys('name', 'commands');
 			expect(result.Test.smtCommands[0].name).to.be.a('string').and.equal('Foo');
-			expect(result.Test.smtCommands[0].commands).to.include('(assert (= a 0))');
+
+			const commands = result.Test.smtCommands[0].commands.split('\n');
+
+			expect(commands[5]).to.include('(assert (= a 0))');
 		});
 
-		it('should make assertions on inverted equalities');
+		it('should make assertions on inverted equalities', () => {
+			const fixture = {
+				Test: {
+					name: 'Test',
+					methods: {
+						Foo: {
+							name: 'Foo',
+							visibility: 'Public',
+							type: 'Integer',
+							arguments: {
+								a: 'Integer',
+								b: 'Integer'
+							},
+							preconditions: [
+								{
+									comparison: 'Equal',
+									arguments: [
+										'a',
+										0
+									],
+									inverted: true,
+									id: '00000000-0000-0000-0000-000000000000'
+								}
+							],
+							postconditions: []
+						}
+					}
+				}
+			};
+			const result = testee(fixture);
+
+			expect(result).to.have.key('Test');
+			expect(result.Test).to.have.keys('name', 'smtCommands');
+			expect(result.Test.smtCommands).to.be.instanceOf(Array).and.have.length(1);
+			expect(result.Test.smtCommands[0]).to.have.keys('name', 'commands');
+			expect(result.Test.smtCommands[0].name).to.be.a('string').and.equal('Foo');
+
+			const commands = result.Test.smtCommands[0].commands.split('\n');
+
+			expect(commands[5]).to.include('(assert (not (= a 0)))');
+		});
+
 		it('should error on making inverted assertions that are not allowed to be inverted');
 		it('should make assertions on standard Numerical logic');
 		it('should make assertions on simple Boolean logic');
