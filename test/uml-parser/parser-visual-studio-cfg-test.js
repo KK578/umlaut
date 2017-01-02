4acc9284f1b532b30f68da5d87ab167ec75ef026
Kevin Kwan <KevinKwan95@googlemail.com>
2017-01-02 19:29:13 +0000

    Add tests for visual-studio condition cfg parser

---
+++ test/uml-parser/parser-visual-studio-cfg-test.js
@@ -0,0 +1,52 @@
+const comparisons = require('../../util/comparisons.json');
+const testee = require('../../uml-parser/parsers/visual-studio/condition-cfg-parser.js');
+
+describe('CFG Parser for Visual Studio Condition Strings', function () {
+	it('should handle a single condition', function () {
+		const result = testee('(Equal a b)');
+
+		expect(result.comparison).to.equal('Equal');
+		expect(result.arguments).to.include('a', 'b');
+		expect(result.exception).to.be.undefined;
+	});
+
+	it('should handle a single condition with an exception', function () {
+		const result = testee('(Equal a b Exception:FooException)');
+
+		expect(result.comparison).to.equal('Equal');
+		expect(result.arguments).to.include('a', 'b');
+		expect(result.exception).to.equal('FooException');
+	});
+
+	it.skip('should handle multiple conditions split by "~~"', function () {
+		const result = testee('(Equal a b)~~(GreaterThan b c)');
+
+		expect(result).to.be.instanceOf(Array).and.have.length(2);
+
+		expect(result[0].comparison).to.equal('Equal');
+		expect(result[0].arguments).to.include('a', 'b');
+		expect(result[1].comparison).to.equal('GreaterThan');
+		expect(result[1].arguments).to.include('b', 'c');
+	});
+
+	describe('Comparisons', function () {
+		comparisons.forEach((comparison) => {
+			it(`should handle comparison named "${comparison.name}"`, function () {
+				const result = testee(`(${comparison.name} a b)`);
+
+				expect(result.comparison).to.equal(comparison.name);
+				expect(result.arguments).to.include('a', 'b');
+			});
+		});
+	});
+
+	describe('Numeric', function () {
+		it('should parse integer values to numbers', function () {
+			const result = testee('(GreaterThan a 0)');
+
+			expect(result.comparison).to.equal('GreaterThan');
+			expect(result.arguments).to.include('a', 0);
+			expect(result.arguments).to.not.include('0');
+		});
+	});
+});
