0f2aed37ae15f2de3567ada8030f16dfbee8036e
Kevin Kwan <KevinKwan95@googlemail.com>
2017-02-14 00:00:25 +0000

    [link-pre-post-conditions] Add LinkedOclCondition class for post conditions

---
+++ uml-annotator/LinkedOclCondition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmlAnnotator
+{
+	class LinkedOclCondition : OclCondition
+	{
+		private static string ConvertString(string condition)
+		{
+			string result = condition;
+
+			// Linked conditions will be present at the start of the condition string, after the '('.
+			// Contained within a { } pair of curly braces.
+			if (result[1] == '{')
+			{
+				result = result.Remove(1, result.IndexOf('}') + 1);
+			}
+
+			return result;
+		}
+
+		private List<OclCondition> linkedConditions;
+
+		public LinkedOclCondition() : base()
+		{
+			linkedConditions = new List<OclCondition>();
+		}
+
+		public LinkedOclCondition(string condition) : base(ConvertString(condition))
+		{
+			linkedConditions = new List<OclCondition>();
+
+			if (condition[1] == '{')
+			{
+				string storedLink = condition.Substring(1, condition.IndexOf('}'));
+			}
+		}
+	}
+}
